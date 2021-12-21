795eb58fbcd8375549e1d79f83e776a2226d12f5
Pitipat <pitipat_fu@cmu.ac.th>
2021-12-21 17:44:34 +0700

    commit Lap12_1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,45 @@
+ #include<iostream>
+ #include<ctime>
+ #include<cstdlib>
+using namespace std ;
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future." ;
+    cin.get();
+    cin.get();
+    cin.get();
+
+    srand(time(0));
+    int A = rand()% 9 + 1 ;
+    if( A == 1 ) cout << "You will get A in this 261102." ;
+    else if (A == 2 ) cout << "You will get B+ in this 261102." ;
+    else if (A == 3 ) cout << "You will get B in this 261102." ;
+    else if (A == 4 ) cout << "You will get C+ in this 261102." ;
+    else if (A == 5 ) cout << "You will get C in this 261102." ;
+    else if (A == 6 ) cout << "You will get D+ in this 261102." ;
+    else if (A == 7 ) cout << "You will get D in this 261102." ;
+    else if (A == 8 ) cout << "You will get F in this 261102." ;
+    else if (A == 9 ) cout << "You will get W in this 261102." ;
+
+    return 0 ;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // cout << "Press Enter 3 times to reveal your future.";
+
 // cout << "You will get A in this 261102.";
